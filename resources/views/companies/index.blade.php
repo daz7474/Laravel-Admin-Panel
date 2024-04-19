58c84735acf63c0b1f9d121c5d2bf54eaaa122b5
Darren Lindsay <daz7474@gmail.com>
2024-04-20 00:37:56 +0100

    Changed image src and classes

--- resources/views/companies/index.blade.php
+++ resources/views/companies/index.blade.php
@@ -1,9 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
+
 <div class="container">
     <h1>Companies</h1>
-    <a href="{{ route('companies.create') }}" class="btn btn-primary my-4">Add New Company</a>
+    <a href="{{ route('companies.create') }}" class="btn btn-primary my-4">+ Add New Company</a>
     <table class="table">
         <thead>
             <tr>
@@ -15,18 +16,19 @@
         </thead>
         <tbody>
             @foreach ($companies as $company)
-            <tr>
+            <tr class="align-middle">
                 <td>{{ $company->name }}</td>
                 <td>{{ $company->email }}</td>
                 <td>
-                    <img src="{{ asset('storage/'.$company->logo) }}" width="100" alt="Company Logo">
+                    <img src="{{ asset('storage/logos/' . $company->logo) }}" width="100" alt="Company Logo" class="rounded">
                 </td>
                 <td>
                     <a href="{{ route('companies.edit', $company) }}" class="btn btn-info">Edit</a>
                     <form action="{{ route('companies.destroy', $company) }}" method="POST" style="display: inline-block;">
                         @csrf
                         @method('DELETE')
-                        <button type="submit" class="btn btn-danger" onclick="return confirm('Are you sure?')">Delete</button>
+                        
+                        <button type="submit" class="btn btn-danger" onclick="return confirm('This will delete all employees associated with this company, continue?')">Delete</button>
                     </form>
                 </td>
             </tr>
