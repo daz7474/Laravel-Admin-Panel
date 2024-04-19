99b85cf10dcd82274eb746478fcde34258a86ade
Darren Lindsay <daz7474@gmail.com>
2024-04-19 17:38:28 +0100

    Added route to controller

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::middleware(['auth'])->group(function () {
+  Route::resource('companies', CompanyController::class);
+  Route::resource('employees', EmployeeController::class);
+});
