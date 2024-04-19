b54ffa79176527fd62b6159915dc86b9d6c66167
Darren Lindsay <daz7474@gmail.com>
2024-04-19 23:11:55 +0100

    Message hide

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
 import './bootstrap';
+
+document.addEventListener('DOMContentLoaded', function () {
+  const alerts = document.querySelectorAll('.alert');
+  alerts.forEach(alert => {
+    setTimeout(() => {
+      alert.style.display = 'none';
+    }, 4000);
+  });
+});
