dc2b5cdcb1c5320f2eb93aaa968a95d5e7b057d6
Darren Lindsay <daz7474@gmail.com>
2024-04-19 18:02:18 +0100

    Initial commit

---
+++ app/Http/Requests/UpdateEmployeeRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class UpdateEmployeeRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
+            'company_id' => 'required|exists:companies,id',
+            'email' => ['nullable', 'string', 'email', 'max:255', Rule::unique('employees')->ignore($this->employee)],
+            'phone' => 'nullable|string|max:255',
+        ];
+    }
+}
