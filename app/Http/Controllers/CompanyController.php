c9b700330ec866c6371fa1a180f55d98c06a9a95
Darren Lindsay <daz7474@gmail.com>
2024-04-19 20:31:26 +0100

    Added Order by name

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -14,7 +14,7 @@ class CompanyController extends Controller
      */
     public function index()
     {
-        $companies = Company::paginate(10);
+        $companies = Company::orderBy('name')->simplePaginate(10);
         return view('companies.index', compact('companies'));
     }
 
