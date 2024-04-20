70ae63c4e3f90c554f0e8aafee2b1b9bb6fa6875
Darren Lindsay <daz7474@gmail.com>
2024-04-20 19:08:48 +0100

    Initial commit

---
+++ resources/views/users/create.blade.php
@@ -0,0 +1,78 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <h1 class="text-center mb-3">New User</h1>
+
+    <div class="d-flex justify-content-center">
+        <form method="POST" action="/users">
+            @csrf
+
+            {{-- Name --}}
+            <div class="form-group mb-3">
+                <label class="mb-2" for="name">Name: </label>
+                <input class="form-control"
+                name="name"
+                id="name"
+                type="text"
+                required
+                >
+
+                @error('name')
+                    <p class="text-danger">{{ $message }}</p>
+                @enderror
+            </div>
+
+            {{-- Email --}}
+            <div class="form-group mb-3">
+                <label class="mb-2" for="email">Email: </label>
+                <input class="form-control"
+                name="email"
+                id="email"
+                type="email"
+                >
+
+                @error('email')
+                    <p class="text-danger">{{ $message }}</p>
+                @enderror
+            </div>
+
+            {{-- Password --}}
+            <div class="form-group mb-3">
+              <label class="mb-2" for="password">Password: </label>
+              <input class="form-control"
+              name="password"
+              id="password"
+              type="password"
+              >
+
+              @error('password')
+                  <p class="text-danger">{{ $message }}</p>
+              @enderror
+          </div>
+
+            {{-- Role --}}
+            <div class="form-group mb-3">
+                <label class="mb-2" for="role">Role: </label>
+                <select class="d-block" name="role" id="role">
+                  @foreach (App\Models\User::roles() as $key => $value)
+                      <option value="{{ $key }}">{{ $value }}</option>
+                  @endforeach
+                </select>
+
+                @error('role')
+                    <p class="text-danger">{{ $message }}</p>
+                @enderror
+            </div>
+
+            <button type="submit" class="btn btn-primary mb-3">
+                Add User
+            </button>
+
+            <div>
+                <a href="/users">Back</a>
+            </div>
+        </form>
+    </div>
+</div>
+@endsection
