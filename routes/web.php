639250886d687cd913e5a8ef9950ee9e970dde7f
Darren Lindsay <daz7474@gmail.com>
2024-04-20 17:43:44 +0100

    Added users route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\CompanyController;
 use App\Http\Controllers\EmployeeController;
 use Illuminate\Support\Facades\Route;
@@ -9,6 +10,7 @@ Auth::routes();
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::middleware(['auth'])->group(function () {
+  Route::resource('users', UserController::class);
   Route::resource('companies', CompanyController::class);
   Route::resource('employees', EmployeeController::class);
 });
