bcaad1fc2248cf69f47314f6d2f0a1fd2da7a0c2
Darren Lindsay <daz7474@gmail.com>
2024-04-29 10:22:29 +0100

    Less margin on card

--- resources/views/employees/show.blade.php
+++ resources/views/employees/show.blade.php
@@ -4,7 +4,7 @@
 
 <div class="container">
     <h1 class="text-center">Employee Details</h1>
-    <div class="card my-5 mx-auto" style="width: 260px;">
+    <div class="card my-4 mx-auto" style="width: 260px;">
       <div class="card-header">
         <h2 class="text-center m-0">{{ "$employee->first_name $employee->last_name" }}</h2>
       </div>
