8f1a60198517f4463951908f6fdd4633b2831c8f
Darren Lindsay <daz7474@gmail.com>
2024-04-29 10:08:43 +0100

    Redesigned layout using flex cards

--- resources/views/employees/show.blade.php
+++ resources/views/employees/show.blade.php
@@ -3,48 +3,44 @@
 @section('content')
 
 <div class="container">
-    <h1>Employee Details</h1>
-    <div class="card my-5">
+    <h1 class="text-center">Employee Details</h1>
+    <div class="card my-5 mx-auto" style="width: 260px;">
       <div class="card-header">
-        <h2 class="m-0">{{ "$employee->first_name $employee->last_name" }}</h2>
+        <h2 class="text-center m-0">{{ "$employee->first_name $employee->last_name" }}</h2>
       </div>
-      <div class="table-responsive">
-        <table class="table text-center">
-          <thead>
-              <tr>
-                  <th>Avatar</th>
-                  <th>Email</th>
-                  <th>Phone Number</th>
-                  <th>Company</th>
-                  @admin
-                  <th>Actions</th>
-                  @endadmin
-              </tr>
-          </thead>
-          <tbody>
-              <tr class="align-middle">
-                  <td><img src="https://i.pravatar.cc/150?u={{ $employee->email }}" alt="Avatar" width="150" height="150" class="rounded-circle"></td>
-                  <td>{{ $employee->email }}</td>
-                  <td>{{ $employee->phone }}</td>
-                  <td>
-                    <a href="/companies/{{ $employee->company->id }}">
-                      {{ $employee->company->name }}
-                    </a>
-                  </td>
-                  <td>
-  
-                      @admin
-                      <a href="{{ route('employees.edit', $employee) }}" class="btn btn-info mb-2">Edit</a>
-                      <form action="{{ route('employees.destroy', $employee) }}" method="POST" style="display: inline-block;">
-                          @csrf
-                          @method('DELETE')
-                          <button type="submit" class="btn btn-danger mb-2" onclick="return confirm('This action is permanent. Are you sure?')">Delete</button>
-                      </form>
-                      @endadmin
-                  </td>
-              </tr>
-          </tbody>
-        </table>
+      <div class="card-body-custom p-2">
+        <img src="https://i.pravatar.cc/150?u={{ $employee->email }}" alt="Avatar" width="150" height="150" class="rounded-circle my-4">
+        <h4>Email:</h4>
+        <p>{{ $employee->email }}</p>
+
+        <h4>Phone Number:</h4>
+        <p>{{ $employee->phone }}</p>
+
+        <h4>Company:</h4>
+        <p>
+          <a href="/companies/{{ $employee->company->id }}">
+            {{ $employee->company->name }}
+          </a>
+        </p>
+
+        @admin
+        <div class="btn-toolbar">
+          <div class="btn-group mx-2">
+            <a href="{{ route('employees.edit', $employee) }}" class="btn btn-info mb-2">
+              Edit
+            </a>
+          </div>
+          <div class="btn-group mx-2">
+            <form action="{{ route('employees.destroy', $employee) }}" method="POST" style="display: inline-block;">
+                @csrf
+                @method('DELETE')
+                <button type="submit" class="btn btn-danger mb-2" onclick="return confirm('This action is permanent. Are you sure?')">
+                  Delete
+                </button>
+            </form>
+          </div>
+        </div>
+        @endadmin
       </div>
     </div>
 </div>
