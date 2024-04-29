6984d6ce520d7a623bfc2dc02e5058accc1d0c24
Darren Lindsay <daz7474@gmail.com>
2024-04-29 11:37:09 +0100

    Removed forelse, used if/else to work with search

--- resources/views/companies/index.blade.php
+++ resources/views/companies/index.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-
 <div class="container">
     <h1>Companies</h1>
     
@@ -16,56 +15,51 @@
         </div>
     </form>
 
-    <div class="table-responsive">
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Website</th>
-                    <th>Logo</th>
-                    <th>Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                @forelse ($companies as $company)
-                <tr class="align-middle">
-                    <td>{{ $company->name }}</td>
-                    <td>{{ $company->email }}</td>
-                    <td>
-                        <a href="{{ Str::startsWith($company->website, ['http://', 'https://']) ? $company->website : 'http://' . $company->website }}" target="_blank">
-                            {{ $company->website }}
-                        </a>
-                    </td>
-                    <td>
-                        <img src="{{ asset('storage/' . $company->logo) }}" width="100" height="60" alt="{{ $company->name }}" class="rounded my-2">
-                    </td>
-                    <td>
-                        <a href="{{ route('companies.show', $company) }}" class="btn btn-warning mb-2">View</a>
-
-                        @admin
-                            <a href="{{ route('companies.edit', $company) }}" class="btn btn-info mb-2">Edit</a>
-                            <form action="{{ route('companies.destroy', $company) }}" method="POST" style="display: inline-block;">
-                                @csrf
-                                @method('DELETE')
-                                
-                                <button type="submit" class="btn btn-danger mb-2" onclick="return confirm('This will delete all employees associated with this company, continue?')">Delete</button>
-                            </form>
-                        @endadmin
-                    </td>
-                </tr>
-                @empty
+    @if ($companies->isEmpty())
+        <p>No companies found.</p>
+    @else
+        <div class="table-responsive">
+            <table class="table">
+                <thead>
                     <tr>
-                        <td colspan="6" class="text-center">There are currently no companies.</td>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Website</th>
+                        <th>Logo</th>
+                        <th>Actions</th>
                     </tr>
-                @endforelse
-            </tbody>
-        </table>
-    </div>
-    
-    {{-- Pagination links --}}
-    <div class="pagination">
-        {!! $companies->links() !!}
-    </div>
+                </thead>
+                <tbody>
+                    @foreach ($companies as $company)
+                    <tr class="align-middle">
+                        <td>{{ $company->name }}</td>
+                        <td>{{ $company->email }}</td>
+                        <td>
+                            <a href="{{ Str::startsWith($company->website, ['http://', 'https://']) ? $company->website : 'http://' . $company->website }}" target="_blank">
+                                {{ $company->website }}
+                            </a>
+                        </td>
+                        <td>
+                            <img src="{{ asset('storage/' . $company->logo) }}" width="100" height="60" alt="{{ $company->name }}" class="rounded my-2">
+                        </td>
+                        <td>
+                            <a href="{{ route('companies.show', $company) }}" class="btn btn-warning mb-2">View</a>
+
+                            @admin
+                                <a href="{{ route('companies.edit', $company) }}" class="btn btn-info mb-2">Edit</a>
+                                <form action="{{ route('companies.destroy', $company) }}" method="POST" style="display: inline-block;">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-danger mb-2" onclick="return confirm('This will delete all employees associated with this company, continue?')">Delete</button>
+                                </form>
+                            @endadmin
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+        {{ $companies->appends(request()->query())->links() }}
+    @endif
 </div>
 @endsection
